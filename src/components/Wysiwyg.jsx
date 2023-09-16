1e3576032c8bd322a2035a20f96033a55205f99d
rabemarcellin <marcellinr.rabe@gmail.com>
2023-09-16 08:06:47 +0300

    add: plain text editor

--- src/components/Wysiwyg.jsx
+++ src/components/Wysiwyg.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useState } from 'react'
 import { GrBold, GrItalic, GrUnderline, GrStrikeThrough,
     GrTextAlignLeft,
     GrTextAlignRight,
@@ -9,8 +9,132 @@ import {
     FaListUl, 
 } from 'react-icons/fa'
 import { TypeAnimation } from 'react-type-animation';
+import { Transforms, Element, createEditor, Editor } from 'slate';
+import { Slate, Editable, withReact } from "slate-react"
 
+const CodeElement = props => {
+    return (
+        <div className='font-title text-sm bg-slate-100 p-2 rounded-md' {...props.attributes}>
+            {props.children}
+        </div>
+    )
+}
 
+const DefaultElement = props => {
+    return (
+        <p {...props.attributes}>{props.children}</p>
+    )
+}
+
+const Leaf = props => {
+    return (
+        <span {...props.attributes}
+        class={`${props.leaf.bold ? 'font-semibold': ''}`}
+        >
+            {props.children}
+        </span>
+    )
+}
+
+const CustomEditor = {
+    isBoldMarkActive(editor) {
+        const marks  = Editor.marks(editor)
+        return marks ? marks.bold === true : false
+    },
+    isCodeBlockActive(editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => {
+                return n.type === "code"
+            }
+        })
+
+        return !!match
+    },
+    toggleBoldMark(editor) {
+        const isActive = CustomEditor.isBoldMarkActive(editor)
+        if(isActive) {
+            Editor.removeMark(editor, 'bold')
+        } else {
+            Editor.addMark(editor, 'bold', true)
+        }
+    },
+    toggleCodeBlock(editor) {
+        const isActive = CustomEditor.isCodeBlockActive(editor)
+        const type = isActive ? null : 'code';
+        Transforms.setNodes(
+                editor,
+                { type: type },
+                { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
+        )
+    }
+}
+
+export const RichEditor = () => {
+    const initialValue = [
+        {
+            type: 'paragraph',
+            children: [{
+                text: 'A line of text in the paragraph'
+            }]
+        }
+    ]
+
+    const renderElement = useCallback(props => {
+        switch(props.element.type) {
+            case 'code':
+                return <CodeElement {...props} />
+            default:
+                return <DefaultElement {...props} />
+        }
+    }) 
+
+    const renderLeaf = useCallback(props => {
+        return (
+            <Leaf {...props} />
+        )
+    })
+
+    const [editor] = useState(withReact(createEditor()))
+
+    return (
+        <div className="flex flex-col my-8 w-full h-[250px] border rounded-md shadow">
+            <div data-type="slate-editor" className='flex-1 p-4 overflow-x-hidden overflow-y-scroll'>
+                <Slate editor={editor} initialValue={initialValue}>
+                <Editable 
+                    renderElement={renderElement}
+                    renderLeaf={renderLeaf}
+                    onKeyDown={event => {
+                        if (!event.ctrlKey) {
+                            return;
+                        }
+                        switch (event.key) {
+                            case "c": {
+                                event.preventDefault()
+                                CustomEditor.toggleCodeBlock(editor)
+                                break;
+                            }
+                            case "b": {
+                                event.preventDefault()
+                                CustomEditor.toggleBoldMark(editor)
+                                break;
+                            }
+                        }
+                    }}
+                />
+                </Slate>
+            </div>
+            <div className="flex-none z-10 bottom-5 w-full left-0 mb-4">
+                <div className="w-11/12 mx-auto bg-slate-100 p-2 border flex gap-2 divide-x justify-center">
+                    <GrBold />
+                    <GrItalic />
+                    <GrUnderline />
+                </div>
+            </div>
+        </div>      
+
+            
+    )
+}
 
 export default function Wysiwyg(demo) {
     const demoText = "OPartage utilise un éditeur de texte riche en fonctionnalité pour vous permettre de styliser le contenu de votre publication."
@@ -36,16 +160,9 @@ export default function Wysiwyg(demo) {
                             <div className="flex  flex-nowrap divide-x divide-black gap-2 border-black rounded-xl font-thin p-2">
                                 {
                                     option.map((Icon, index) => (
-                                        index !== 2 ? (
-                                            <button className='active:scale-95 hover:text-gray-500 text-center'>
-                                                <Icon key={index} className='ml-2' />
-                                            </button>
-                                        ):(
-                                            <button className='hidden md:block active:scale-95 hover:text-gray-500 text-center'>
-                                                <Icon key={index} className='ml-2' />
-                                            </button>
-                                        )
-                                       
+                                        <button className='hidden md:block active:scale-95 hover:text-gray-500 text-center'>
+                                            <Icon key={index} className='ml-2' />
+                                        </button>
                                     ))
                                 }
                             </div>
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,10 +7,11 @@ import { PreviewDesktopImg, PreviewMobileImg, StarImg } from "../assets";
 // Components
 import Footer from "../components/layouts/Footer";
 import Linebreak from "../components/Linebreak";
-import Wysiwyg from "../components/Wysiwyg";
-    
+import Wysiwyg, { RichEditor } from "../components/Wysiwyg";
+import { TypeAnimation } from "react-type-animation";
 
 export default function Landing() {
+    const demoText = "OPartage utilise un éditeur de texte riche en fonctionnalité pour vous permettre de styliser le contenu de votre publication."
 
     useEffect(() => {
         document.title = "OPartage - Acceuil";
@@ -32,29 +33,38 @@ export default function Landing() {
                 </div>
             </div>
             <Linebreak />
-            <div className="px-4 lg:px-20 lg:grid grid-cols-2 gap-20 my-10">
-                <div className="mb-8 lg:mb-0">
+            <div className="px-4 lg:px-20 grid lg:grid-cols-2 lg:gap-20 my-10">
+                <div className="order-2 lg:order-1">
+                    <RichEditor />
+                </div>
+                <div className="order-1 lg:mb-0">
                     <div className="flex flex-col items-center">
                         <h1 className="font-title text-3xl lg:text-4xl mb-6">Appyue sur tes mots</h1>
-                        <Wysiwyg demo />
+                            <TypeAnimation 
+                            wrapper='div'
+                                speed={50}
+                                repeat={0}
+                                sequence={[demoText, 1000]}
+                        />
                     </div>
-                    
                 </div>
-                <div className="relative mb-8 md:mb-0 text-center lg:text-start">
-                        <h1 className="text-3xl lg:text-4xl font-title">
-                            Balance tout d'une seule fois
-                        </h1>
-                        <p className="my-6">
-                            Un utilisateur n'est autorisé qu'à un
-                            commentaire sur une publication. Adieu l'occasion
-                            des petites querelles
-                        </p>
-                        <div className="absolute right-0 -translate-x-16">
-                            <div className="w-10 h-100">
-                                <img src={StarImg} alt="etoile" />
-                            </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-8 md:mb-0 text-center">
+                    <h1 className="text-3xl lg:text-4xl font-title">
+                        Balance tout d'une seule fois
+                    </h1>
+                    <p className="my-6">
+                        Un utilisateur n'est autorisé qu'à un
+                        commentaire sur une publication. Adieu l'occasion
+                        des petites querelles
+                    </p>
+                    <div className="absolute right-0 -translate-x-16">
+                        <div className="w-10 h-100">
+                            <img src={StarImg} alt="etoile" />
                         </div>
                     </div>
+                </div>
             </div>
             <div className="flex justify-center items-center my-10 ">
                 <div className="border-2 border-slate-800">
